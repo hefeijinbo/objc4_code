202383206c34e2564845667d157068c8d7e6b077
jinbo03 <jinbo03@baidu.com>
2023-07-23 08:10:33 +0800

    main.cpp

---
+++ ODDemo/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ODDemo
+//
+//  Created by jinbo03 on 2023/7/23.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject {
+    int age;
+}
+
+@end
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
